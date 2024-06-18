e5ad0cedc1e1eab4e85a61cca17fa22727e0c458
fisp <arenatkanovna@gmail.com>
2024-06-18 23:38:32 +0300

    Добавьте файлы проекта.

---
+++ Task1/Program.cs
@@ -0,0 +1,35 @@
+﻿using Task1;
+
+Order order = new();
+order.Print();
+Console.WriteLine("_____________");
+
+Console.WriteLine("Operators:"); //вывод операторов
+
+Order order1 = new() { Summ = 0 };
+Console.WriteLine(order1.Summ);
+order1++;
+order1.Print();
+
+Console.WriteLine("________________________________"); //вывод заказа
+Order order2 = new() { Summ = 2000, Address = "ул. Пушкина д. Колотушкино", Name = "Илья" };
+Order order3 = new() { Summ = 500 };
+Console.WriteLine(order2.Summ);
+Console.WriteLine(order3.Summ);
+
+
+Order order4 = order2 + order3;
+Console.WriteLine(order4.Summ);
+order4.Print();
+
+Console.WriteLine("______________________________"); //анализ заказов на 'и' и 'или'
+
+Console.WriteLine("______________________________");
+Order order5 = new() { Summ = 100 };
+Console.WriteLine(order5.Summ);
+
+
+if (order5) //анализ пятого заказа
+    Console.WriteLine("Сумма заказа больше или равно 0 (>=0)");
+else
+    Console.WriteLine("сумма заказа меньше 0 (<0)");
---
+++ Task1/class.cs
@@ -0,0 +1,45 @@
+﻿namespace Task1
+{
+    public class Order  //имена компонентов класса
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public int Summ { get; set; }
+
+        public Order() //первоначальный класс
+           : this("Неизвестна","Неизвестен",0) { }
+
+        public Order(string Name, string Address, int Summ) //присваивание переменным служебные имена
+        {
+            this.Name = Name;
+            this.Address = Address;
+            this.Summ = Summ;
+        }
+
+        public static Order operator ++(Order order) 
+            => new Order
+            {
+                Name = order.Name,
+                Address = order.Address,
+                Summ = order.Summ + 1
+            };
+
+        public static Order operator +(Order first, Order second) =>
+            new Order //присваивание из Program.CS имён в класс
+            {
+                Name = first.Name,
+                Address = first.Address,
+                Summ = first.Summ + second.Summ
+            };
+
+        //рассчёты операторов
+        public static bool operator true(Order order) => order.Summ >= 0;
+
+        public static bool operator false(Order order) => order.Summ < 0;
+
+        public void Print() //вывод класса
+        {
+            Console.WriteLine($"Заказ: {Name}, адрес: {Address}, Сумма: {Summ}");
+        }
+    }
+}
